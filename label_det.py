b0f2be4906eea5c9888caefb4680b5b88fe9f961
bw4sz <benweinstein2010@gmail.com>
2017-04-04 12:45:52 -0700

    successful annotation and credentials

---
+++ label_det.py
@@ -0,0 +1,76 @@
+import argparse
+import httplib2
+import time
+import json
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+#export GOOGLE_APPLICATION_CREDENTIALS="C:/Users/Ben/Dropbox/Google/MeerkatReader-9fbf10d1e30c.json"
+
+API_DISCOVERY_FILE = 'video-intelligence-service-discovery-v1beta1.json'
+OPERATIONS_DISCOVERY_FILE = 'video-intelligence-operations-discovery.json'
+
+def main(gcs_uri):
+
+    credentials = GoogleCredentials.get_application_default()
+
+    with open(API_DISCOVERY_FILE, 'r') as f:
+        doc = f.read()
+    video_service = discovery.build_from_document(
+        doc, credentials=credentials)
+
+    with open(OPERATIONS_DISCOVERY_FILE, 'r') as f:
+        op_doc = f.read()
+    op_service = discovery.build_from_document(
+        op_doc, credentials=credentials)
+
+    video_service_request = video_service.videos().annotate(
+        body={
+            'inputUri': gcs_uri,
+            'features': ['LABEL_DETECTION']
+        })
+
+    response = video_service_request.execute()
+    name = response['name']
+
+    op_service_request = op_service.operations().get(name=name)
+    response = op_service_request.execute()
+    op_start_time = str(response['metadata']['annotationProgress'][0]['startTime'])
+    print('Operation {} started: {}'.format(name, op_start_time))
+
+    while True:
+        response = op_service_request.execute()
+        time.sleep(30)
+        if 'done' in response and response['done'] == True:
+            break
+        else:
+            print('Operation processing ...')
+    print('The video has been successfully processed.')
+    print('')
+    labelData = response['response']['annotationResults'][0]['labelAnnotations']
+    print ('Video Annotations:')
+    for label in labelData:
+        if 'locations' not in label:
+            print ('Error in label detection: ' + label['description'])
+        else:
+            print label['description']
+            locations = label['locations']
+            for location in locations:
+                if 'segment' not in location:
+                    print 'Missing segment.'
+                else:
+                    segment = location['segment']
+                    startTime = segment.get('startTimeOffset', '0')
+                    endTime = segment.get('endTimeOffset', '0')
+                    print "  " + startTime + ", " + endTime
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-gcs_uri', help='The Google Cloud Storage URI of the video.',default='gs://api-project-773889352370-ml/Videos/jantest.mp4')
+    args = parser.parse_args()
+    main(args.gcs_uri)
+    
+    
+

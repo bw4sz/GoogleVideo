4e228b861769399783267a291fa8a0173f578705
bw4sz <benweinstein2010@gmail.com>
2017-04-04 16:13:09 -0700

    testing structure, couldn't be bothered with the relative import from a seperate tests folder, sorry python heroes.

--- MotionMeerkat/tests_basic.py
+++ MotionMeerkat/tests_basic.py
@@ -1,8 +1,8 @@
-from context import MotionMeerkat
+import MotionMeerkat
 import os
 
 #Run defaults
 # Serice account credentials
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/Ben/Dropbox/Google/MeerkatReader-9fbf10d1e30c.json"
 
-print MotionMeerkat.__path__
+MotionMeerkat.MotionMeerkat()

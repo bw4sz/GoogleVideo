9880bfab7fa5096e56fad1a2b13f2ae388d28a21
bw4sz <benweinstein2010@gmail.com>
2017-04-04 19:39:51 -0700

    working on playing video, added test video to deal with downloading later

--- MotionMeerkat/Video.py
+++ MotionMeerkat/Video.py
@@ -1,5 +1,7 @@
 import label
 import time
+import cv2
+import numpy
 
 class Video:
     def __init__(self,path):
@@ -8,5 +10,31 @@ class Video:
     def label(self):
         labels=label.main(self.path)
         label.label_parse(labels)
+    def download(self):
+        #check if path exists
+        #if not download file
+        vidname=os.path.basename(path)
+        print "Checking if %s exists" %(vidname)
+        self.local_file="staging/" + vidname
+        if not os.path.isfile(self.local_file):
+            print "Downloading..."
+            #download
+    def write(self):
+        
+        #play video
+        cap = cv2.VideoCapture(self.local_file)
+        
+        while(cap.isOpened()):
+            ret, frame = cap.read()        
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+        
+        
+        
+        
         
         
